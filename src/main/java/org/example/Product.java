dbab24e19aa4850c13465782d151f4ca7794da86
victoriaromangarrido <victoria.roman@estudiantat.upc.edu>
2022-10-03 12:53:21 +0200

    Tot creat

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Product {
+    public String getProductId() {
+    }
+
+    public double getPrice() {
+    }
+
+    public String getDescription() {
+    }
+
+    public int getNumSales() {
+    }
+}
