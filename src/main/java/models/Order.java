3ac1f9968bc2e35143a388f077e8a0002a14d63b
CarlaVictorio <101456362+CarlaVictorio@users.noreply.github.com>
2022-10-17 19:53:42 +0200

    17.10.22

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,9 +1,12 @@
-package org.example;
+package models;
 
 public class Order {
     public void addLP(int i, String b001) {
     }
 
     public Object getLP(int i) {
+    }
+
+    public String getUserId() {
     }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -6,5 +6,10 @@ public class User {
     private String userID;
     private String name;
     private String surname;
+
+    public void addOrder(Order o) {
+    }
+
+
     private List<>
 }
